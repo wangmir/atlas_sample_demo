31bd143b2bfd4608ba77bb9e5a59f36bc6da93a7
Hyukjoong Chuck Kim <wangmir@gmail.com>
2023-03-21 18:17:23 -0700

    Add Sample table which is established with composite key.
    Signed-off-by: Hyukjoong Chuck Kim <wangmir@gmail.com>

---
+++ schema/migrations/20230322011704.sql
@@ -0,0 +1,2 @@
+-- Create "test_new_composite_table" table
+CREATE TABLE "testschema"."test_new_composite_table" ("name" character varying(255) NOT NULL, "sample_column_1" character varying(255) NOT NULL, PRIMARY KEY ("name", "sample_column_1"));
